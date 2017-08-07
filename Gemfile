640612d0b840f80d55fb9996249819943efeb287
kardashovdmitry <kardashovdima@gmail.com>
2017-08-07 10:00:40 +0300

    Add secure password

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'uglifier', '>= 1.3.0'
 
 gem 'jquery-rails'
 
+gem 'bcrypt', '~> 3.1.7'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804125616) do
+ActiveRecord::Schema.define(version: 20170807065104) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170804125616) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
